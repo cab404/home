0c7ca94a2904e200fd10b0034a362331c8bc743a
Vladimir Serov <me@cab404.ru>
2019-10-04 16:38:10 +0300

    [feat] templates: userconf.nix

---
+++ templates/userconf.nix
@@ -0,0 +1,22 @@
+# This file is overlayed on top of the main config to personalize it.
+{ ... }: {
+  home.username = "john";
+
+  programs.git = {
+    userName = "John Smith";
+    userEmail = "john@example.com";
+    signing = {
+      key = "1BB96810926F4E715DEF567E6BA7C26C3FDF7BB4";
+      signByDefault = true;
+    };
+  };
+
+  programs.ssh.matchBlocks = let 
+    is = (user: identityFile: { inherit user identityFile; });
+  in
+  {
+    # You can list your ssh key to hostname associations here.
+    "example.com" = is "john" "~/.ssh/id_rsa";
+  };
+
+}
