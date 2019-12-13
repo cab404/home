556a0d339e0f1108646e4bd8a5d8e11e09aef753
Vladimir Serov <me@cab404.ru>
2019-12-14 02:00:50 +0300

    [feat] dell 5400: trackpoint works like a scroll wheel

--- hw/dell-latitude-5400.nix
+++ hw/dell-latitude-5400.nix
@@ -1,9 +1,13 @@
 {...}: {
-  hardware = {
-    trackpoint = {
-      device = "DELL08B8:00 0488:121F Mouse";
-      enable = true;
-      emulateWheel = true;
-    };
-  };
+  # Trackpoint scroll fix. Injection!
+  services.xserver.libinput.additionalOptions = ''
+EndSection
+Section "InputClass"
+  Identifier "libinput pointer catchall"
+  MatchIsPointer "on"
+  MatchDevicePath "/dev/input/event*"
+  Driver "libinput"
+  Option "ScrollMethod" "button"
+  Option "ScrollButton" "2"
+  '';
 }
