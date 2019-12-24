c6f58294d363817f596138d8aae7ee72d0eed159
Vladimir Serov <me@cab404.ru>
2019-12-25 02:12:04 +0300

    [feat] yay, nocaps at tty

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,12 @@ in
   ];
 
   # Time
-  i18n = { consoleFont = "Lat2-Terminus16"; consoleKeyMap = "us"; defaultLocale = "en_US.UTF-8"; };
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    defaultLocale = "en_US.UTF-8";
+    consoleUseXkbConfig = true; # ctrl:nocaps at last
+  };
+
   time.timeZone = "Europe/Moscow";
 
   nix = {
