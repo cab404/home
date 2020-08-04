81d20973c7df47ed44bb57d3cc87ea979e98fa78
Vladimir Serov <me@cab404.ru>
2020-08-04 17:44:55 +0300

    home-manager config from system config is fixed, but I don't use it rn

--- home-manager.nix
+++ home-manager.nix
@@ -1,16 +1,13 @@
-{ pkgs, ... }:
-let
-  home-manager = builtins.fetchGit {
-    url = "https://github.com/rycee/home-manager.git";
-    rev = "621c98f15a31e7f0c1389f69aaacd0ac267ce29e";
-  };
-  evalHomeConfig = import "${home-manager}/modules";
-in
+{ pkgs, config, ... }:
 {
-  # imports = [ "${home-manager}/nixos" ];
+  # imports = [ <home-manager/nixos> ];
   # home-manager.useUserPackages = true;
-  # home-manager.users.cab = builtins.removeAttrs (evalHomeConfig {
-  #     configuration = ./home.nix;
-  #     inherit pkgs;
-  # }).config [ "_module" "assertions" "home" "submoduleSupport"];
+  # home-manager.users = {
+  #   # "${config._.user}" = {
+  #   #   imports = [ ./home.nix ];
+  #   # };
+  #   root = {
+  #     imports = [ ./home.nix ];
+  #   };
+  # };
 }
