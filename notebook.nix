1e1ec302c4deebbec5cab8215cc0dd401b7a87ca
Vladimir Serov <me@cab404.ru>
2020-08-25 04:53:46 +0300

    moved opencl stuff into hw specific stuff

--- notebook.nix
+++ notebook.nix
@@ -8,15 +8,4 @@ _: {
     # ./home-manager.nix
   ];
 
-  hardware.opengl = let
-    opencl_pr = import (builtins.fetchTarball {
-      name = "opencl_pr";
-      url = "https://github.com/athas/nixpkgs/archive/f92a2a9b69eba9909d25ffaab6ded4d6f0f4efad.tar.gz";
-    }) { };
-  in {
-    enable = true;
-    driSupport32Bit = true;
-    package = opencl_pr.mesa.drivers;
-    extraPackages = [ opencl_pr.mesa ];
-  };
 }
