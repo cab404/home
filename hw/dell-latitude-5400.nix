c8940d99e81edd3130728a46ff4f4ed3b7d4fbd0
Vladimir Serov <me@cab404.ru>
2020-09-19 18:48:48 +0300

    doesn't work with flakes :(

--- hw/dell-latitude-5400.nix
+++ hw/dell-latitude-5400.nix
@@ -24,21 +24,11 @@ Section "InputClass"
     #"iwlwifi.power_level=5" # ITS OVER 4!
   ];
 
-
-  # OpenCL from [this](https://github.com/NixOS/nixpkgs/pull/82729) PR
-  hardware.opengl = let
-    # opencl_pr = import (builtins.fetchTarball {
-    #   name = "opencl_pr";
-    #   url = "https://github.com/athas/nixpkgs/archive/f92a2a9b69eba9909d25ffaab6ded4d6f0f4efad.tar.gz";
-    #   sha256 = "1yf3w0k5iqslimnir5zznjn8rpzq9nb51nrab75kklklsmrnlx8h";
-    # }) { inherit currentSystem };
-  in {
+  hardware.opengl = {
     enable = true;
     driSupport32Bit = true;
-    # package = opencl_pr.mesa.drivers;
     extraPackages = with pkgs; [
       beignet
-      # opencl_pr.mesa
       clblas
     ];
   };
