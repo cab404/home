956d7a7813920068bf9b0d8a4b4d33c5f8f8f4d7
Vladimir Serov <me@cab404.ru>
2021-10-30 15:10:38 +0300

    OH GOD, nixpkgs-fmt >:(

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,83 +1,86 @@
 /**
-* Desktop related configuration.
-* Desktop is all that is not a server .-.
+  * Desktop related configuration.
+  * Desktop is all that is not a server .-.
 */
-{ config, pkgs, lib, ... }:
-let
-  enableThings = with builtins;
-    things: overrides:
-    if length things == 0
-    then overrides
-    else lib.recursiveUpdate (enableThings (tail things) overrides) {
-      "${head things}".enable = true;
-    };
-
-  # I want warnings on unfree software, but I won't stay completely free, if
-  # that shatters my productivity
-  forgiveMeStallman = package: package.overrideAttrs(a: { meta = {}; });
-in
+{ config, pkgs, lib, ... }@args:
+with import ../lib.nix args;
 {
 
   require = [ ./. ];
 
   documentation = {
     dev.enable = true;
-    # makes all the external modules break builds :/ # nixos.includeAllModules = true;
+    # makes all the external modules break builds :/
+    # nixos.includeAllModules = true;
   };
-
-  xdg = enableThings [ "portal" ] {
-  };
-
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    # It doesn't build :| (forgiveMeStallman amdgpu-pro)
-  ];
 
   nixpkgs.config = {
     checkMeta = true;
     android_sdk.accept_license = true;
   };
 
-  fonts.fontconfig.enable = true;
+  # Can't enable flatpak without this
+  xdg.portal = on;
 
-  services = enableThings [
-    "ntp" "locate" "xserver"
-    "actkbd" "flatpak"
-    "tor" "gnunet" "earlyoom" "printing"
-  ] {
+  # @balsoft's hack to enable battery levels on supported headphones
+  systemd.services.bluetooth.serviceConfig.ExecStart = lib.mkForce [ "" "${pkgs.bluezFull}/libexec/bluetooth/bluetoothd -f /etc/bluetooth/main.conf -E" ];
 
-    printing.drivers = [
-      pkgs.gutenprint
-    ];
+  services = {
 
-    logind = {
-      lidSwitch = "suspend-then-hibernate";
-      lidSwitchExternalPower = "suspend";
+    # gnunet = e;
+    locate = on;
+    upower = on;
+    flatpak = on;
+
+    pipewire = on // {
+      jack = on;
+      alsa = on;
+      pulse = on;
+      media-session = on;
     };
 
-    actkbd = {
-      bindings = [
-        {keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 2"; }
-        {keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 2"; }
+    printing = on // {
+      drivers = [
+        pkgs.gutenprint
       ];
     };
+
+    logind = {
+      lidSwitch = "suspend";
+      lidSwitchExternalPower = "suspend";
+      extraConfig = ''
+        # IdleAction=lock
+        # IdleActionSec=30
+        HandlePowerKey=suspend
+      '';
+    };
+
+    actkbd = on // {
+      bindings = [
+        { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 2"; }
+        { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 2"; }
+        # that's pretty much ctrlaltdel on steroids - lalt + super + ralt + rctrl + delete
+        { keys = [ 56 97 100 111 125 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/killall -9 sway"; }
+      ];
+    };
+
     udev.packages = [
       pkgs.android-udev-rules
       pkgs.stlink
       pkgs.openocd
       (pkgs.writeTextDir "/etc/udev/rules.d/42-user-devices.rules" ''
-      # Saleae Logic thing
-      ATTR{idVendor}=="0925", ATTR{idProduct}=="3881", GROUP+="dialout"
-      # USBTiny
-      ATTR{idVendor}=="1781", ATTR{idProduct}=="0c9f", GROUP+="dialout"
+        # Saleae Logic thing
+        ATTR{idVendor}=="0925", ATTR{idProduct}=="3881", GROUP+="dialout"
+        # USBTiny
+        ATTR{idVendor}=="1781", ATTR{idProduct}=="0c9f", GROUP+="dialout"
       '')
     ];
 
-    earlyoom.freeMemThreshold = 5;
+    earlyoom = on // { freeMemThreshold = 5; };
 
-    tor = {
-      controlSocket.enable = true;
-      client.enable = true;
-      client.dns.enable = true;
+    tor = on // {
+      client = on // { dns = on; };
+      controlSocket = on;
     };
 
   };
@@ -85,39 +88,36 @@ in
   # == Sound
   sound.enable = true;
   hardware = {
-    opengl.enable = true;
-    pulseaudio = {
-      enable = true;
-      extraModules = with pkgs; [
-        pulseaudio-modules-bt
-      ];
-      package = pkgs.pulseaudioFull;
-      zeroconf.discovery.enable = true;
-      daemon.config = {
-        flat-volumes = "no";
-      };
-    };
-    bluetooth.enable = true;
+    opengl = on;
+    bluetooth = on;
+    opentabletdriver = on;
   };
 
   # virtualisation.anbox.enable = true;
-  virtualisation.docker.enable = true;
+  virtualisation.podman = on;
 
   programs = enableThings [
     "light" # brightness control
     # "plotinus" # command pallet that doesn't work yet for some reason
     "wireshark" # should create some missing groups
-  ] {};
+  ]
+    { };
 
   powerManagement.powertop.enable = lib.mkDefault true;
 
   environment.systemPackages = with pkgs; [
-    gnome3.dconf xfce.xfconf  # programs <3 configs
+    gnome3.dconf
+    xfce.xfconf # programs <3 configs
   ];
 
   users.users."${config._.user}".extraGroups = [
-    "docker" "containers" "plugdev"
-    "tor" "wireshark" "libvirtd"
+    "docker"
+    "containers"
+    "plugdev"
+    "tor"
+    "wireshark"
+    "libvirtd"
+    "sound"
   ];
 
 }
