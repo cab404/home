8c360f5b53debd1b5320acdb2a9b6d6303144fc0
Vladimir Serov <me@cab404.ru>
2021-10-30 15:09:39 +0300

    oh, yeah, lib got better

--- lib.nix
+++ lib.nix
@@ -1,11 +1,13 @@
-{ pkgs, ... }: let
+{ lib, ... }: let
   enableThings = with builtins;
     things: overrides:
     if length things == 0
     then overrides
-    else pkgs.lib.recursiveUpdate (enableThings (tail things) overrides) {
+    else lib.recursiveUpdate (enableThings (tail things) overrides) {
       "${head things}".enable = true;
-    };
+  };
+  on = { enable = true; };
+  forgiveMeStallman = package: package.overrideAttrs(a: { meta = {}; });
 in {
-  inherit enableThings;
+  inherit enableThings on forgiveMeStallman;
 }
