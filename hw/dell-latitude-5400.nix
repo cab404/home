da253ff7e7d828cedf5a0f4b177ec9966534e647
Vladimir Serov <me@cab404.ru>
2021-11-22 01:38:08 +0300

    un-fixed it.

--- hw/dell-latitude-5400.nix
+++ hw/dell-latitude-5400.nix
@@ -1,32 +1,31 @@
 { pkgs, ... }: {
   # Trackpoint scroll fix. Injection!
-  services.xserver.libinput.touchpad.additionalOptions = ''
-EndSection
-Section "InputClass"
-  Identifier "libinput pointer catchall"
-  MatchIsPointer "on"
-  MatchDevicePath "/dev/input/event*"
-  Driver "libinput"
-  Option "ScrollMethod" "button"
-  Option "ScrollButton" "2"
-  '';
+#  services.xserver.libinput.touchpad.additionalOptions = ''
+#EndSection
+#Section "InputClass"
+#  Identifier "libinput pointer catchall"
+#  MatchIsPointer "on"
+#  MatchDevicePath "/dev/input/event*"
+#  Driver "libinput"
+#  Option "ScrollMethod" "button"
+#  Option "ScrollButton" "2"
+#  '';
 
   services.tlp.enable = true;
   services.blueman.enable = true;
 
   boot.kernelParams = [
     #"amdgpu.runpm=0" # disable power manager
-    "amdgpu.gpu_recovery=1" # mowmow, hang in there
-    "amdgpu.job_hang_limit=100" # trying to get it not to hang
-    # "amdgpu.deep_color=1" # y not?
-    "amdgpu.exp_hw_support=1" # mow
+    #"amdgpu.gpu_recovery=1" # mowmow, hang in there
+    #"amdgpu.job_hang_limit=100" # trying to get it not to hang
+    #"amdgpu.deep_color=1" # y not?
+    #"amdgpu.exp_hw_support=1" # mow
 
     "i915.enable_gvt=1" # virtal graphics
     "i915.error_capture=1" # they dies sometimes
 
     #"iwlwifi.power_level=5" # ITS OVER 4!
   ];
-
   boot.blacklistedKernelModules = [ "amdgpu" ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
