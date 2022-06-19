7ab36556d816efc31e4040592ef1a75f7f3c85b9
cab <cab404@mailbox.org>
2022-06-20 03:02:49 +0400

    help nixfmt ate my config

--- hw/portables/yuna/home.nix
+++ hw/portables/yuna/home.nix
@@ -1,9 +1,9 @@
-args @ { sysconfig, config, pkgs /* inputs.nixpkgs */, lib /* inputs.nixpkgs.lib */, inputs, ... }:
+args@{ sysconfig, config, pkgs # inputs.nixpkgs
+, lib # inputs.nixpkgs.lib
+, inputs, ... }:
 with import ../../../lib.nix args;
-let
-  isWL = true;
-in
-{
+let isWL = true;
+in {
 
   imports = [
     inputs.nix-doom-emacs.hmModule
@@ -15,152 +15,147 @@ in
 
   home = {
 
-    packages = with pkgs; [
+    packages = with pkgs;
+      [
 
-      # Interweebs
-      transmission-gtk
-      thunderbird
-      # jitsi-meet-electron
-      tdesktop
-      mumble
-      ungoogled-chromium
+        # Interweebs
+        transmission-gtk
+        thunderbird
+        # jitsi-meet-electron
+        tdesktop
+        mumble
+        ungoogled-chromium
 
-      (if isWL then element-desktop-wayland else element-desktop)
+        (if isWL then element-desktop-wayland else element-desktop)
 
-      # Coding
-      #vscodium #imdone #yolo #sorryrms
-      rnix-lsp
-      gh
-      ghc
-      jdk8
-      nim
-      julia-stable-bin # all julias are generally broken. which strangely coinsides with my life experience
+        # Coding
+        rnix-lsp
+        gh
+        ghc
+        jdk8
+        nim
+        julia-stable-bin # all julias are generally broken. which strangely coinsides with my life experience
 
-      # Editing
-      libreoffice
-      inkscape
-      gimp
-      krita
-      ffmpeg-full
-      peek
-      audacity
-      bat
-      openscad
-      solvespace
-      blender
-      simple-scan
-      lmms
+        # Editing
+        libreoffice
+        inkscape
+        gimp
+        krita
+        ffmpeg-full
+        peek
+        audacity
+        bat
+        openscad
+        solvespace
+        blender
+        simple-scan
+        lmms
 
-      # at least do backups
-      restic
+        # at least do backups
+        restic
 
-      # Fonts
-      source-code-pro
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      roboto
-      fira-code
-      fira
-      font-awesome
-      gohufont
+        # Fonts
+        source-code-pro
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        roboto
+        fira-code
+        fira
+        font-awesome
+        gohufont
 
-      # Window manager and looks stuff
-      helvum
-      qrencode
+        # Window manager and looks stuff
+        helvum
+        qrencode
 
-      # Command line comfort
-      alacritty
-      zsh
-      findutils
-      pulsemixer
-      silver-searcher
-      fzf
-      file
-      jq
-      jless
-      ranger
-      btop
+        # Command line comfort
+        alacritty
+        zsh
+        findutils
+        pulsemixer
+        silver-searcher
+        fzf
+        file
+        jq
+        jless
+        ranger
+        btop
 
-      # Runners
-      appimage-run
-      lutris-free
+        # Runners
+        appimage-run
+        lutris-free
 
-      # Development
-      docker-compose
-      insomnia
-      remmina
+        # Development
+        docker-compose
+        insomnia
+        remmina
 
-      # Hardware?
-      minicom
-      pulseview
-      cutecom
-      picocom
-      scrcpy
-      qFlipper
-      ledger-live-desktop
+        # Hardware?
+        minicom
+        pulseview
+        cutecom
+        picocom
+        scrcpy
+        qFlipper
+        #ledger-live-desktop
 
-      # Viewers
-      feh
-      mpv
-      zathura
-      font-manager
-      gthumb
-      audacious
-      quodlibet
-      ytfzf
+        # Viewers
+        feh
+        mpv
+        zathura
+        font-manager
+        gthumb
+        audacious
+        quodlibet
+        ytfzf
 
+      ]
+      # Mate
+      ++ (with mate; [ mate-calc caja ])
 
-    ]
-    # Mate
-     ++ (with mate; [
-      mate-calc
-      caja
-    ])
+      # Gnome
+      ++ (with gnome; [
+        gnome-disk-utility
+        gnome-power-manager
+        gnome-sound-recorder
+        seahorse
+        file-roller
+        gucharmap
+        baobab
+        evince
+        sushi
+      ]) ++ [
+        gnome-online-accounts
 
-    # Gnome
-    ++ (with gnome; [
-      gnome-disk-utility
-      gnome-power-manager
-      gnome-sound-recorder
-      seahorse
-      file-roller
-      gucharmap
-      baobab
-      evince
-      sushi
-    ]) ++ [
-      gnome-online-accounts
+        # Funny utilities
+        aircrack-ng
+        netsniff-ng
+        netdiscover
+        hashcat
+        wireshark
+        mtr
+        strace
+        hcxdumptool
+        sshfs
 
-      # Funny utilities
-      aircrack-ng
-      netsniff-ng
-      netdiscover
-      hashcat
-      wireshark
-      mtr
-      strace
-      hcxdumptool
-      sshfs
+        # Personal data and sync
+        browserpass
+        gnupg
+        nextcloud-client
 
-      # Personal data and sync
-      browserpass
-      gnupg
-      nextcloud-client
+        # Themes, all of them
+        adwaita-qt
+        gnome3.adwaita-icon-theme
+        gnome-themes-extra
 
-      # Themes, all of them
-      adwaita-qt
-      gnome3.adwaita-icon-theme
-      gnome-themes-extra
+        # Notes are important!
+        (writeShellScriptBin "notes" ''
+          codium ~/data/cab/notes/ ~/data/cab/notes/$(date +%Y-%m-%d).md
+        '')
 
-      # Notes are important!
-      (writeShellScriptBin "notes" ''
-        codium ~/data/cab/notes/ ~/data/cab/notes/$(date +%Y-%m-%d).md
-      '')
-
-      (
-        let
+        (let
           conf = builtins.toFile "woficonf" ''
             filter_rate=200
             dynamic_lines=true
@@ -168,8 +163,7 @@ in
             matching=fuzzy
             layer=overlay
           '';
-        in
-        (writeShellScriptBin "wofi-pass" ''
+        in (writeShellScriptBin "wofi-pass" ''
           WOFI=${wofi}/bin/wofi
           WCONF="-c ${conf}"
           set -e
@@ -177,59 +171,62 @@ in
           SELECTION=$((cd $PASSWORD_STORE_DIR; find -type f -not -path './.*' | sed 's/.gpg$//') | $WOFI $WCONF --show dmenu)
           echo pass --clip $MODE $SELECTION
           pass $MODE -c $SELECTION
+        ''))
+
+        # like which, but for nix
+        (writeShellScriptBin "what" ''
+          readlink -f $(which $@)
         '')
-      )
 
-      # like which, but for nix
-      (writeShellScriptBin "what" ''
-        readlink -f $(which $@)
-      '')
+        # I would like to have my hotspot now
+        (writeShellScriptBin "nm-rescan" ''
+          nmcli dev wifi rescan
+        '')
 
-      # I would like to have my hotspot now
-      (writeShellScriptBin "nm-rescan" ''
-        nmcli dev wifi rescan
-      '')
+        (pkgs.callPackage ../../../theme-changer.nix { })
 
-      (pkgs.callPackage ../../../theme-changer.nix { })
+        # nix search, but without the internets
+        (writeShellScriptBin "nix-search" ''
+          nix search --offline nixpkgs $@
+        '')
 
-      (writeShellScriptBin "nix-search" ''
-        nix search --offline nixpkgs $@
-      '')
+        # Blocking emacs.
+        (writeShellScriptBin "ee" ''
+          ${emacs}/bin/emacsclient -c $@
+        '')
 
-      # Blocking emacs.
-      (writeShellScriptBin "ee" ''
-        ${emacs}/bin/emacsclient -c $@
-      '')
+        # Non-blocking emacs
+        (writeShellScriptBin "ec" ''
+          ${emacs}/bin/emacsclient -nc $@
+        '')
 
-      # Non-blocking emacs
-      (writeShellScriptBin "ec" ''
-        ${emacs}/bin/emacsclient -nc $@
-      '')
+      ];
 
-    ];
-
-    file.".mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/lib/mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+    file.".mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json".source =
+      "${pkgs.plasma-browser-integration}/lib/mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json";
 
     # == Keyboard config
     keyboard = {
       layout = sysconfig.services.xserver.layout;
-      options = with builtins; filter isString (split "," sysconfig.services.xserver.xkbOptions);
+      options = with builtins;
+        filter isString (split "," sysconfig.services.xserver.xkbOptions);
     };
 
     sessionVariables = {
       XCOMPOSEFILE = "${config.xdg.configHome}/XCompose";
       XKB_DEFAULT_LAYOUT = config.home.keyboard.layout;
-      XKB_DEFAULT_OPTIONS = pkgs.lib.concatStringsSep "," config.home.keyboard.options;
+      XKB_DEFAULT_OPTIONS =
+        pkgs.lib.concatStringsSep "," config.home.keyboard.options;
     };
   };
 
-  xdg.configFile =
-    let composeConfig = ''
-      include "${pkgs.xorg.libX11}/share/X11/locale/en_US.UTF-8/Compose"
-      <Multi_key> <period> <backslash>           : "λ"   U03BB  # GREEK SMALL LETTER LAMBDA
-    '';
-    in
-    {
+  xdg = on // {
+    configFile = let
+      composeConfig = ''
+        include "${pkgs.xorg.libX11}/share/X11/locale/en_US.UTF-8/Compose"
+        <Multi_key> <period> <backslash>           : "λ"   U03BB  # GREEK SMALL LETTER LAMBDA
+      '';
+    in {
       # you probably wonder, "how did you find this?"
       # it's obvious: gtk3 sources.
       # also, it won't work in gtk2.
@@ -238,7 +235,7 @@ in
       # And this one I found in tdesktop. Cause why not? At least it's XDG Compliant (tm)
       "XCompose".text = composeConfig;
     };
-
+  };
   # don't know where to put it
   fonts.fontconfig.enable = true;
 
@@ -251,90 +248,83 @@ in
     "exa"
     "bat"
     "doom-emacs"
-  ]
-    {
+  ] {
 
-      doom-emacs = {
-        doomPrivateDir = ../../../doom.d;
-      };
+    doom-emacs = { doomPrivateDir = ../../../doom.d; };
 
-      vscode = on // {
-        package = pkgs.vscodium;
-      };
+    vscode = on // { package = pkgs.vscodium; };
 
-      firefox.package = pkgs.firefox-esr-unwrapped;
+    firefox.package = pkgs.firefox-esr-unwrapped;
 
-      exa.enableAliases = true;
+    exa.enableAliases = true;
 
-      password-store = {
-        package = pkgs.pass.withExtensions (e: with e; [
-          pass-otp
-          pass-update
-          pass-genphrase
-          pass-audit
-        ]);
-      };
-
-      # == SSH
-      ssh = {
-        compression = true;
-        matchBlocks =
-          let
-            is = (user: identityFile: { inherit user identityFile; });
-          in
-          {
-            "*.serokell.team" = {
-              port = 17788;
-              user = "cab404";
-            };
-
-            "cab404.ru" = is "cab" "~/.ssh/id_rsa";
-          };
-      };
-
-      # == Pass and stuff
-      browserpass.browsers = [ "firefox" ];
-
-      git = {
-        userName = "Vladimir Serov";
-        userEmail = "me@cab404.ru";
-        extraConfig = {
-          pull.ff = "only";
-          init = {
-            defaultBranch = "master";
-          };
-        };
-        signing = {
-          key = "1BB96810926F4E715DEF567E6BA7C26C3FDF7BB3";
-          signByDefault = true;
-        };
-      };
-
-
+    password-store = {
+      package = pkgs.pass.withExtensions
+        (e: with e; [ pass-otp pass-update pass-genphrase pass-audit ]);
     };
 
+    # == SSH
+    ssh = {
+      compression = true;
+      matchBlocks =
+        let is = (user: identityFile: { inherit user identityFile; });
+        in {
+          "*.serokell.team" = {
+            port = 17788;
+            user = "cab404";
+          };
+
+          "cab404.ru" = is "cab" "~/.ssh/id_rsa";
+        };
+    };
+
+    # == Pass and stuff
+    browserpass.browsers = [ "firefox" ];
+
+    git = {
+      userName = "Vladimir Serov";
+      userEmail = "me@cab404.ru";
+      extraConfig = {
+        pull.ff = "only";
+        init = { defaultBranch = "master"; };
+      };
+      signing = {
+        key = "1BB96810926F4E715DEF567E6BA7C26C3FDF7BB3";
+        signByDefault = true;
+      };
+    };
+
+  };
+
   services = enableThings [
-    "gpg-agent"
     "pass-secret-service"
     "password-store-sync"
     "flameshot"
     "easyeffects"
     "kdeconnect"
-    # "emacs"
-  ]
-    {
-      gpg-agent = {
-        enableSshSupport = true;
-        sshKeys = [
-          "28D5BB057E5E743B9917335CDA8F71D89506FF7F"
-          "AB76EEA25B5E957595B61C28F5A81F597C44A711"
-        ];
+    "emacs"
+  ] {
+    gpg-agent = on // {
+      enableSshSupport = true;
+      sshKeys = [
+        "28D5BB057E5E743B9917335CDA8F71D89506FF7F"
+        "AB76EEA25B5E957595B61C28F5A81F597C44A711"
+      ];
+    };
+
+    git-sync = on // {
+      repositories = {
+        notes = {
+          path = "/home/cab/data/cab/notes";
+          uri = "git@git.sr.ht:~cab/notes";
+          interval = 120;
+        };
       };
     };
 
+  };
+
   # == Gnome hates when there's no dconf -.-
-  dconf.enable = true;
-  # long time brewing
-  xdg.enable = true;
+  dconf = on;
 
 }
