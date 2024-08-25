ea16b7cae8737dc03c8407d743372f1b9b292f01
Cabia Rangris <cab@cab.moe>
2024-08-25 18:24:53 +0400

    sound got deprecated

--- modules/recipes/audio.nix
+++ modules/recipes/audio.nix
@@ -1,9 +1,8 @@
 args@{ inputs, prelude, lib, config, pkgs, ... }: with prelude; let __findFile = prelude.__findFile; in {
 
   # == Sound
-  sound.enable = true;
   security.rtkit = on;
-  
+
   # == You won't believe how I found this :D
   networking.firewall.allowedUDPPorts = [ 6001 6002 ];
   # == okay, you would . but it was not obvious!
@@ -24,7 +23,7 @@ args@{ inputs, prelude, lib, config, pkgs, ... }: with prelude; let __findFile =
             "context.modules" = [
               # Config to make pipewire discover stuff around it with zeroconf.
               { name = "libpipewire-module-zeroconf-discover"; }
-              { name = "libpipewire-module-raop-discover"; 
+              { name = "libpipewire-module-raop-discover";
                 raop.autoreconnect = true;
               }
             ];
