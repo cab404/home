d0092ed9004283debd7d78c00eaeaabf31d5f745
Cabia Rangris <cab@cab.moe>
2024-08-25 18:22:28 +0400

    sort of return of the sessionvars

--- modules/kde/home.nix
+++ modules/kde/home.nix
@@ -21,13 +21,13 @@
 
   # systemd.user.services.copyq.Service.Environment = lib.mkForce [ "QT_QPA_PLATFORM=wayland" ];
 
-  # systemd.user.sessionVariables = {
+  systemd.user.sessionVariables = {
 
   #   # okay, okay, whatever.
   #   QT_QPA_PLATFORM = "wayland";
 
-  #   # for some reason it doesn't want to read anything else
-  #   SDL_VIDEODRIVER = "wayland";
+    # for some reason it doesn't want to read anything else
+    SDL_VIDEODRIVER = "wayland";
 
   #   # self-descriptive
   #   MOZ_ENABLE_WAYLAND = "1";
@@ -35,14 +35,14 @@
   #   # actually I'm sure it's set somewhere else too
   #   GDK_BACKEND = "wayland";
   #   # DISPLAY = "_";
-  #   # woo wrappers
-  #   NIXOS_OZONE_WL = "1";
+    # woo wrappers
+    NIXOS_OZONE_WL = "1";
 
   #   QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
 
-  #   # Fixing java apps (especially idea)
-  #   _JAVA_AWT_WM_NONREPARENTING = "1";
+    # Fixing java apps (especially idea)
+    _JAVA_AWT_WM_NONREPARENTING = "1";
 
-  # };
+  };
 
 }
