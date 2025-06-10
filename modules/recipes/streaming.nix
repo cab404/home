1c0adda91bd279555cb1593da9370ec321379c63
Cabia Rangris <cab@cab.moe>
2025-06-10 22:07:09 +0200

    cab: haha funy cam

--- modules/recipes/streaming.nix
+++ modules/recipes/streaming.nix
@@ -5,4 +5,7 @@ with prelude; let __findFile = prelude.__findFile; in
   programs.gphoto2 = on;
   users.users.cab.extraGroups = [ "camera" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="Funny Cam" exclusive_caps=1
+  '';
 }
