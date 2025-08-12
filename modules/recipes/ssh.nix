21518ef19849e501252ea1dd5e760f443f540a1d
Cabia Rangris <cab@cab.moe>
2025-08-13 00:26:09 +0400

    weather update ssh implies mosh

--- modules/recipes/ssh.nix
+++ modules/recipes/ssh.nix
@@ -3,6 +3,9 @@ let keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIKIABDEIeccdbZwTgxhkVUIyZa8fx9uyiE0I2S9t4x1 cab404@meow2"
 ]; in
 {
+
+  programs.mosh.enable = true;
+
   services.openssh = {
     enable = true;
     settings = { PasswordAuthentication = false; };
