98b311e7a2cff12dfe5b090fb23bd8fa0a520d64
Cabia Rangris <cab@cab.moe>
2025-08-13 01:02:14 +0400

    i swear that barecore is not some kind of kink

--- modules/barecore.nix
+++ modules/barecore.nix
@@ -2,108 +2,113 @@
 
 { config, pkgs, lib, prelude, ... }: with prelude; let __findFile = prelude.__findFile; in {
 
-  # ====== Packages
-
-  environment.defaultPackages = (with pkgs; [
-    # this section is a tribute to my PEP-8 hatred
-    curl htop git tmux  # why aren't those there by default?
-    killall usbutils pciutils zip unzip # WHY AREN'T THOSE THERE BY DEFAULT?
-    nmap arp-scan rsync
-
-    helix vim
-
-    nix-index # woo, search in nix packages files!
-
-    # nix-zsh-completions
-    # zsh-completions # systemctl ena<TAB>... AAAAGH
-    # nix-bash-completions
-    # bash-completion
-    mosh
-
-    waypipe
-  ]);
-
   # ====== NixOS system-level stuff
-
   system.stateVersion = "23.11";
-
-  # In the grim dark future there is only NixOS
-  # system.stateVersion = "40000.00";
-  # (enables all of the unstable features pretty much always)
-
   require = [ ./options.nix ];
-  nix = {
-    package = pkgs.nixVersions.latest;
-    settings = {
-      trusted-users = [ "root" config._.user ];
-      experimental-features = [ "nix-command" "flakes" "ca-derivations" ];
-    };
-
-    # This pins nixpkgs from the flake.lock system-wide both in registry and NIX_PATH
-    nixPath = [ "nixpkgs=${pkgs.path}" ];
-    registry =
-      let
-        lock = (with builtins; fromJSON (readFile ../flake.lock));
-      in
-      {
-        nixpkgs =  lib.mkForce (with lock.nodes.${lock.nodes.${lock.root}.inputs.nixpkgs}; {
-          from = { id = "nixpkgs"; type = "indirect"; };
-          to = locked;
-        });
-      };
-  };
-
-  # ====== User configuration
-
-  users = {
-    mutableUsers = false;
-    users."${config._.user}" = {
-      isNormalUser = true;
-      extraGroups = [
-        "wheel"
-        "containers"
-        "networkmanager"
-        "plugdev"
-        "dialout"
-        "video"
-      ];
-      shell = pkgs.nushell;
-    };
-    users.root.shell = pkgs.nushell;
-    defaultUserShell = pkgs.nushell;
-  };
-
-  # ====== Kernel
-
-  boot = lib.mkDefault {
-    kernelParams = [ "quiet" ];
-  };
 
   i18n.defaultLocale = "C.UTF-8";
 
-  services.xserver.xkb = {
-    layout = "us,ru";
-    options = "ctrl:nocaps,lv3:ralt_switch_multikey,misc:typo,grp:rctrl_switch";
-  };
+  imports = [
+    <modules/recipes/ssh.nix>
 
-  programs = {
-    zsh = on // {
-      enableCompletion = true;
-    };
-    bash.completion.enable = true;
-  };
+    # ====== User configuration
+    {
+        users = {
+            mutableUsers = false;
+            users."${config._.user}" = {
+                isNormalUser = true;
+                extraGroups = [
+                    "wheel"
+                    "containers"
+                    "networkmanager"
+                    "plugdev"
+                    "dialout"
+                    "video"
+                ];
+                shell = pkgs.nushell;
+            };
+            users.root.shell = pkgs.nushell;
+            defaultUserShell = pkgs.nushell;
+        };
 
-  environment.variables = { EDITOR = "hx"; };
+        programs = {
+          zsh = on // {
+            enableCompletion = true;
+          };
+          bash.completion.enable = true;
+        };
 
+        environment.variables = { EDITOR = "hx"; };
 
-  # ====== Core services
-  programs.mosh.enable = true;
-  services = {
+    }
+    # ====== Console
+    {
+        console = {
+            colors = [
+                "3A3C43" "BE3E48" "869A3A" "C4A535"
+                "4E76A1" "855B8D" "568EA3" "B8BCB9"
+                "888987" "FB001E" "0E712E" "C37033"
+                "176CE3" "FB0067" "2D6F6C" "FCFFB8"
+            ];
+        };
+    }
+    # ====== Keyboard
+    {
+      services.xserver.xkb = {
+        layout = "us,ru";
+        options = "ctrl:nocaps,lv3:ralt_switch_multikey,misc:typo,grp:rctrl_switch";
+      };
+      console = {
+        packages = [ pkgs.kbd ];
+        useXkbConfig = true; # ctrl:nocaps at last
+      };
+    }
+    # ====== Nix config stuff
+    {
+      nix = {
+        package = pkgs.nixVersions.latest;
+        settings = {
+          trusted-users = [ "root" config._.user ];
+          experimental-features = [ "nix-command" "flakes" "ca-derivations" ];
+        };
+      };
+    }
+    # ====== Nixpkgs registry hack^W pinning
+    {
+      nix = {
+        # This pins nixpkgs from the flake.lock system-wide both in registry and NIX_PATH
+        nixPath = [ "nixpkgs=${pkgs.path}" ];
+        registry =
+          let
+            lock = (with builtins; fromJSON (readFile ../flake.lock));
+          in
+          {
+            nixpkgs =  lib.mkForce (with lock.nodes.${lock.nodes.${lock.root}.inputs.nixpkgs}; {
+              from = { id = "nixpkgs"; type = "indirect"; };
+              to = locked;
+            });
+          };
+      };
+    }
 
-    openssh = on // {
-      settings.PasswordAuthentication = false;
-    };
+    # ====== Packages
+    {
 
-  };
+      environment.defaultPackages = (with pkgs; [
+        # this section is a tribute to my PEP-8 hatred
+        curl htop git tmux  # why aren't those there by default?
+        killall usbutils pciutils zip unzip # WHY AREN'T THOSE THERE BY DEFAULT?
+        nmap arp-scan rsync
+        waypipe # way to ssh -X, but wayland
+
+        helix vim # Vim is rarely needed, but still needed sometimes
+
+        nix-index # woo, search in nix packages files!
+
+      ]);
+
+    }
+
+  ];
 
 }
--- modules/core.nix
+++ modules/core.nix
@@ -14,43 +14,8 @@
   ]);
 
   documentation.man = {
-    generateCaches = true;
+    # generateCaches = true;
   };
-
-  # ====== NixOS system-level stuff
-
-  # In the grim dark future there is only NixOS
-  # system.stateVersion = "40000.00";
-  # (enables all of the unstable features pretty much always)
-
-  # ====== Basic tty and shell look-and-feel configuration and hacks
-
-  console = {
-    colors = [
-      "3A3C43" "BE3E48" "869A3A" "C4A535"
-      "4E76A1" "855B8D" "568EA3" "B8BCB9"
-      "888987" "FB001E" "0E712E" "C37033"
-      "176CE3" "FB0067" "2D6F6C" "FCFFB8"
-    ];
-    packages = [ pkgs.kbd ];
-    # Is executed during phase 1 :(
-    # font = "Lat2-Terminus16";
-    useXkbConfig = true; # ctrl:nocaps at last
-  };
-  # well, i think I need it in the same location I have it inside initrd for
-  # vconsole setup to properly work, and I don't want to make filtered package
-  # with one font for that to reduce initrd closure size. and I am not feeling like
-  # increasing initrd closure size by kbd.out number of megabytes
-  # boot.initrd.extraFiles."/share/consolefonts/Lat2-Terminus16.psfu.gz" = "${pkgs.kbd}/share/consolefonts/Lat2-Terminus16.psfu.gz";
-
-
-  i18n.defaultLocale = "C.UTF-8";
-
-  services.xserver.xkb = {
-    layout = "us,ru";
-    options = "ctrl:nocaps,lv3:ralt_switch_multikey,misc:typo,grp:rctrl_switch";
-  };
-
 
   # ====== Security keys support
 
@@ -64,7 +29,7 @@
 
   services = {
 
-    # avahi = on;
+    avahi = on;
     fwupd = on // {
       extraRemotes = [ "lvfs-testing" ];
     };
