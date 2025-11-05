2b2aded674ed740692420c1ee73db3751063f0fc
Cabia Rangris <cab@cab.moe>
2025-11-05 13:59:14 +0400

    tailscale changed again

--- nodes/keter/tiferet/configuration.nix
+++ nodes/keter/tiferet/configuration.nix
@@ -57,33 +57,29 @@ with prelude; let __findFile = prelude.__findFile; in
 
   services.caddy = on // {
     virtualHosts = {
-      # "gtch.cab.moe" = {
-      #   extraConfig = ''
-      #     handle_path /static/* {
-      #       root * ${inputs.gtch.packages.x86_64-linux.static}
-      #       file_server
-      #     }
-      #     reverse_proxy 10.0.10.1:8000
-      #   '';
-      # };
+      "gtch.cab.moe" = {
+        extraConfig = ''
+          reverse_proxy c1:8011
+        '';
+      };
       "eris.cab.moe" = {
         extraConfig = ''
-          reverse_proxy eris.keter:6006
+          reverse_proxy eris:6006
         '';
       };
       "pretix.cab.moe" = {
         extraConfig = ''
-          reverse_proxy twob.keter:8345
+          reverse_proxy twob:8345
         '';
       };
       "immich.cab.moe" = {
         extraConfig = ''
-          reverse_proxy c1.keter:2283
+          reverse_proxy c1:2283
         '';
       };
       "paperless.cab.moe" = {
         extraConfig = ''
-          reverse_proxy c1.keter:8000
+          reverse_proxy c1:8000
         '';
       };
       "static.cab.moe" = {
@@ -92,20 +88,20 @@ with prelude; let __findFile = prelude.__findFile; in
           file_server browse
         '';
       };
-      "ocapn.cab.moe" = {
-        extraConfig = ''
-          reverse_proxy http://c1.keter:7000
-        '';
-      };
-      "nextcloud.cab.moe" = {
-        extraConfig = ''
-          @webdav {
-            path_regexp N /.well-known/(card|cal)dav
-          }
-          rewrite @webdav /remote.php/dav/
-          reverse_proxy http://c1.keter:80
-        '';
-      };
+      # "ocapn.cab.moe" = {
+      #   extraConfig = ''
+      #     reverse_proxy http://c1.keter:7000
+      #   '';
+      # };
+      # "nextcloud.cab.moe" = {
+      #   extraConfig = ''
+      #     @webdav {
+      #       path_regexp N /.well-known/(card|cal)dav
+      #     }
+      #     rewrite @webdav /remote.php/dav/
+      #     reverse_proxy http://c1.keter:80
+      #   '';
+      # };
       # "nextcloud.cab404.ru" = {
       #   extraConfig = ''
       #     @webdav {
