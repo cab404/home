387e69816b498829641f94cd186893853d109a2c
Cabia Rangris <cab@cab.moe>
2026-09-09 00:04:47 +0400

    flowey

---
+++ nodes/eris/howdy.nix
@@ -0,0 +1,7 @@
+{
+  security.pam.howdy.enable = true;
+  services.howdy.enable = true;
+  services.linux-enable-ir-emitter.enable = true;
+  services.howdy.control = "sufficient";
+  services.howdy.settings.video.dark_threshold = 110;
+}
