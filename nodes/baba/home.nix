021fdb7af1ec7bea8e2e0f07ad8b356a3bbc900d
Cabia Rangris <cab@cab.moe>
2026-09-08 13:46:13 +0400

    btw my framework exploded

--- nodes/baba/home.nix
+++ nodes/baba/home.nix
@@ -8,15 +8,4 @@ let __findFile = prelude.__findFile; in {
     <modules/home-dumpster.nix>
   ];
 
-  wayland.windowManager.sway.config = {
-    input."2362:628:PIXA3854:00_093A:0274_Touchpad" = {
-      # dwt = "disabled";
-      tap = "enabled";
-      pointer_accel = "0.5";
-    };
-    output."eDP-1" = {
-      scale = "1";
-    };
-  };
-
 }
